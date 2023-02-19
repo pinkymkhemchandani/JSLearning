d8d09f28281ff8518eeccf98ba239b7837fc5cd7
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-02-19 14:55:21 +0530

    01_variable_assign.js
    01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+console.log("****** step 1 *****");
+var myFullName
+console.log(myFullName);
+myFullName = "myFullName is pinky khemchandani";
+console.log(myFullName);
+myFullName = "myFullName is pinky mulchand khemchandani";
+console.log(myFullName);
+
+console.log("****** step 2 *****");
+var myLove = " i love only JS";
+console.log(myLove);
+
+console.log("****** step 3 *****");
+
+var firstName = " firstName pinky";
+var lastName = " lastName khemchandani";
+var age = " age 27";
+var address = " address veraval";
+var pincode = " pincode 362265";
+var gender = " gender female";
+var isMarried = " isMarried no";
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
+
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,19 @@
 console.log("pinky khemchandani");
+console.log("31");
+console.log("veraval");
+
+var fullName = "pinky kehmchandani"
+console.log(fullName);
+console.log(fullName);
+
+var city; //variable declaration
+console.log(city);
+city ="veraval"; //variable initialization
+console.log(city);
+city ="surat";
+console.log(city); 
+
+var age ="31"; //varibale declaration and initialiazation at the same line
+age= 32;
+age = 33;
+console.log(age);
