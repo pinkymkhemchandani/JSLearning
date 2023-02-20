3f1e7e4d1afad3f3618906d63697dab3b4411480
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-02-20 10:43:03 +0530

    swapVariable.js
    swapVariable.js

---
+++ 02_swapVariables.js
@@ -0,0 +1,9 @@
+var sweety= "sweety"
+console.log(sweety);
+var Cutie = "cutie"
+console.log(Cutie);
+sweety = Cutie
+console.log(sweety);
+console.log(Cutie);
+var temp = "sweety"
+console.log(temp);
