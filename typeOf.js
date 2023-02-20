37eb8c705a8410021ae0c9c4e3d70ba755446f9a
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-02-20 10:33:05 +0530

    typeOf.js
    typeOf.js

---
+++ typeOf.js
@@ -0,0 +1,19 @@
+console.log("*****1******");
+var bankName;
+console.log(typeof bankName);
+bankName = "SBI Bank"
+console.log("Bank name is:", bankName , " ,type of bankNAme is:" , typeof bankName);
+console.log("****2****");
+var marks = 90
+console.log("marks:",marks, ", type of marks is:", typeof marks);
+ console.log("***3****");
+ var isWorking;
+ console.log(isWorking);
+ isWorking=true
+console.log("isWorking:", isWorking , ",Type of isWorking:", typeof isWorking);
+console.log("****4.a****");
+var totalCount="Hundred and Seven"
+console.log("totalCount:",totalCount, "Type of totalCount:",typeof totalCount);
+console.log("****4.b****");
+var totalCount=107
+console.log("totalCount:",totalCount, "Type of totalCount:",typeof totalCount);
