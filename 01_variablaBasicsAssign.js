48fcdccdf356f16de8ac66e74810024c6a1dd604
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-02-20 09:58:18 +0530

    variableBasicsAssign.js
    variableBasicsAssign.js

---
+++ 01_variablaBasicsAssign.js
@@ -0,0 +1,25 @@
+console.log("*****number*****");
+var age= 27
+console.log("type of variable is:", typeof age);
+var pincode= 362265
+console.log("type of variable is:", typeof pincode);
+var members= 80
+console.log("type of variable is:", typeof members);
+
+console.log("*****string*****");
+var myName = "pinky"
+console.log("type of variable is:",typeof myName);
+var lastName = "khemchandani"
+console.log("type of variable is:",typeof lastName);
+var middleName = "mulchand"
+console.log("type of variable is:",typeof middleName);
+
+console.log("*****boolean*****");
+var isMarried = false
+console.log("type of variable is:", typeof isMarried);
+var isLazy = true
+console.log("type of variable is:", typeof isLazy);
+var isExcited = true
+console.log("type of variable is:", typeof isExcited);
+
+
