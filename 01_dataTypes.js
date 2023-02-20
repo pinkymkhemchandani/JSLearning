b730cb6e5a7bb846b8a9043dc8b42b0b85f6a890
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-02-20 21:53:21 +0530

    dataTypes.js
    dataTypes.js

---
+++ 01_dataTypes.js
@@ -0,0 +1,35 @@
+var myName = "virat kohli";//string
+var age = 31; // integer
+var weight = 60.5 //integer
+
+var isMarried = true; //boolean
+var isLazy = false; //boolean
+
+var typeOfmyName = typeof myName;
+console.log("Typeof varibale myName:",typeOfmyName);
+console.log("typeOf variable fatherName:",typeof fatherNAme );
+
+console.log("typeof age is:",typeof age);
+console.log("typeof isMarried is:",typeof isMarried);
+
+var graduation;
+console.log("value of graduation variable is:",graduation);
+console.log("typeof graduation is:",typeof graduation);
+
+var city = null;
+console.log("value of city variable is:",city);
+console.log("typeof city is:",typeof city);
+
+age = "32";
+console.log(typeof age);
+
+
+
+age = 100;
+console.log(typeof age);
+
+age = "true";
+console.log(typeof age);
+
+age = false;
+console.log(typeof age);
