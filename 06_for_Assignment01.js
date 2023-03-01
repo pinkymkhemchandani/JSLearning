bb7a42cac5e898a40b1952dbda459565e9678117
pinkymkhemchandani <pinkymkhemchandani@gmail.com>
2023-03-01 19:51:58 +0530

    forLoop
    forLoop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,42 @@
+console.log(`WAP to print numbers from 5 to 15 by increment 1`);
+for (let index = 5; index <=15; index=index+1) {
+    console.log(index);;
+    
+}
+
+console.log(`WAP to print numbers from 50 to 40 by decrement 1`);
+for (let index = 50 ; index >= 40 ; index=index-1) {
+    console.log(index);
+    
+}
+
+console.log(`WAP to find first 15 odd numbers`);
+for (let index = 1; index <= 30; index=index+2) {
+    console.log(index);
+    
+}
+
+console.log(`WAP to find first 10 even numbers`);
+for (let index = 0; index >= 20; index=index+2) {
+    console.log(index);
+    
+}
+
+console.log(`WAP to print table of 5`);
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log(`WAP to print table of 10`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+    
+}
+
+console.log(`WAP to print table of 10 in reverse`);
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+    
+}
+
